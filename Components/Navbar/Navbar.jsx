10e823d9c3f948f78cf6375d52bfdb12d8e9d5cf
Reshmashanmugam1510 <160820034+Reshmashanmugam1510@users.noreply.github.com>
2024-12-01 12:00:00 +0530

    updated

--- Components/Navbar/Navbar.jsx
+++ Components/Navbar/Navbar.jsx
@@ -3,17 +3,7 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div>
- <div className="comtainer">
-    <h1>
-        home
-    </h1>
- </div>
- <div className="comtainer">
-    <h1>
-        home
-    </h1>
- </div>
-      <p>Hello</p>
+        
     </div>
   )
 }
